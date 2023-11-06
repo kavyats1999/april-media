878e8a73b8a128d9b54f0e43275d8df97ca23f79
kavyats1999 <tskavyasajeev@gmail.com>
2023-11-06 14:58:50 +0530

    media player backend

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// import json server
+
+const jsonserver = require('json-server')
+
+// create json server
+
+const server = jsonserver.create()
+
+//route json data
+
+const router=jsonserver.router('db.json')
+
+// create middle ware
+
+const middleware = jsonserver.defaults()
+
+//port
+
+const PORT = process.env.PORT || 4000
+
+server.use(middleware)
+server.use(router)
+
+
+server.listen(PORT, () => {
+    console.log("media player server started at port:" + PORT);
+})
+
